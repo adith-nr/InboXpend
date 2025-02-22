1457d995c7329e85f8d4c39d593a9c8c25f8c6bb
23abdul23 <23abdulazeem23@gmail.com>
2025-02-22 15:32:03 +0530

    Run the new code and add data since 15th Jan, and then upload the new files. Then I will make the further fucntions

--- read.py
+++ read.py
@@ -3,6 +3,7 @@ import email
 from email.header import decode_header
 from bs4 import BeautifulSoup  
 import numpy as np
+import pandas as pd
 import json
 
 
@@ -68,35 +69,48 @@ def FindCostFromGivenDate(dt):
                     if("DEBITED for Rs" in i):
                         
                        #  cost = (i.split("DEBITED for Rs.")[1].split()[0].replace(",",""))
-                         details = (i.split("DEBITED for Rs.")[1].split())
-                         cost = float(details[0].replace(",",""))
-                         Upi_id=details[1].upper()
-                         #print(Upi_id)
-                         Upi_id=Upi_id.split(':')[2].split("(")[0].split('-')[0]
-                       
-                         transac_id = details[1].split(':')[1]
-                         if(transac_id in transac_ids):
-                              continue
-                         transac_ids.add(transac_id)
-                         #print(Upi_id)
-                         if(Upi_id=='Q793458026@YBL'):
-                              NC_Cost+=cost
-                         elif(Upi_id=='PAYTMQR65NEBI@PTYS' or Upi_id=='PAYTMQR281005050101OHZ7AF0MP5V0@'):
-                              Nescafe_Cost+=cost
-                         elif(Upi_id=='PAYTMQR65MYTO@PTYS' or Upi_id=='PAYTMQR5ZIYAE@PTYS'):
-                              Gupta_cost+=cost
-                             
-                         elif(Upi_id=='8858420752'):
-                               Amul_cost+=cost
-                         
-                       
-                        #  balance = details[-1].split('Rs.')[1]
-                        #  balance=balance.split('.')[0]
-                       
+                        details = (i.split("DEBITED for Rs.")[1].split())
+                        cost = float(details[0].replace(",",""))
+                        Upi_id=details[1].upper()
+                        #print(Upi_id)
+                        Upi_id=Upi_id.split(':')[2].split("(")[0].split('-')[0]
+                    
+                        transac_id = details[1].split(':')[1]
+
+                        Shop_name = ''
+
+                        if(transac_id in transac_ids):
+                            continue
+                        transac_ids.add(transac_id)
+                        #print(Upi_id)
+                        if(Upi_id=='Q793458026@YBL'):
+                            NC_Cost+=cost
+                            Shop_name = "Night_Canteen"
+                        elif(Upi_id=='PAYTMQR65NEBI@PTYS' or Upi_id=='PAYTMQR281005050101OHZ7AF0MP5V0@'):
+                            Nescafe_Cost+=cost
+                            Shop_name = "Nescafe"
+                        elif(Upi_id=='PAYTMQR65MYTO@PTYS' or Upi_id=='PAYTMQR5ZIYAE@PTYS'):
+                            Gupta_cost+=cost
+                            Shop_name = "Gupta_Ji"
+                            
+                        elif(Upi_id=='8858420752'):
+                            Amul_cost+=cost
+                            Shop_name = "Amul"
                         
-                         AllCost.append(cost)
-                         TotalCost+=cost
-                         break
+                    
+                    #  balance = details[-1].split('Rs.')[1]
+                    #  balance=balance.split('.')[0]
+
+                        df = pd.read_csv('database.csv')
+                        df["Date"] = pd.to_datetime(df['Date'])
+                        new_row = {'Date': date, 'UPI_id': Upi_id, 'Shop': Shop_name, 'Amount': cost}
+                        df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+                        df = df.sort_values(by='Date')
+                        df.to_csv('database.csv', index=False)
+
+                        AllCost.append(cost)
+                        TotalCost+=cost
+                        break
                
                 
                
